ecb91c845e1409e2af5f694c24978017d41c2924
Rafael Bemerguy <rafabemerguy@gmail.com>
2015-06-19 21:39:29 -0300

    Correções IDEA

--- src/main/java/br/com/listadetarefas/beans/Tarefa.java
+++ src/main/java/br/com/listadetarefas/beans/Tarefa.java
@@ -1,61 +1,94 @@
 package br.com.listadetarefas.beans;
 
-import java.util.Calendar;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Calendar;
 
 public class Tarefa {
-	private Long id;
+    private Long id;
 
-	@NotNull(message="{tarefa.descricao.vazia}")
-	@Size(min=5, message="{tarefa.descricao.pequena}")
-	private String descricao;
+    @NotNull(message = "{tarefa.descricao.vazia}")
+    @Size(min = 5, message = "{tarefa.descricao.pequena}")
+    private String descricao;
+    private boolean finalizado;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Calendar dataFinalizacao;
 
-	private boolean finalizado;
+    public Tarefa() {
+        this.finalizado = false;
+    }
 
-	@DateTimeFormat(pattern="dd/MM/yyyy")
-	private Calendar dataFinalizacao;
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("descricao", descricao)
+                .append("finalizado", finalizado)
+                .append("dataFinalizacao", dataFinalizacao)
+                .toString();
+    }
 
-	public Tarefa() {
-		this.finalizado = false;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-	public Long getId() {
-		return id;
-	}
+        Tarefa tarefa = (Tarefa) o;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+        if (finalizado != tarefa.finalizado) return false;
+        if (dataFinalizacao != null ? !dataFinalizacao.equals(tarefa.dataFinalizacao) : tarefa.dataFinalizacao != null)
+            return false;
+        if (!descricao.equals(tarefa.descricao)) return false;
+        if (id != null ? !id.equals(tarefa.id) : tarefa.id != null) return false;
 
-	public String getDescricao() {
-		return descricao;
-	}
+        return true;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + descricao.hashCode();
+        result = 31 * result + (finalizado ? 1 : 0);
+        result = 31 * result + (dataFinalizacao != null ? dataFinalizacao.hashCode() : 0);
+        return result;
+    }
 
-	public boolean isFinalizado() {
-		return finalizado;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setFinalizado(boolean finalizado) {
-		this.finalizado = finalizado;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Calendar getDataFinalizacao() {
-		return dataFinalizacao;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
-	public void setDataFinalizacao(Calendar dataFinalizacao) {
-		if(dataFinalizacao != null) {
-			finalizado = true;
-		}
-		this.dataFinalizacao = dataFinalizacao;
-	}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isFinalizado() {
+        return finalizado;
+    }
+
+    public void setFinalizado(boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+
+    public Calendar getDataFinalizacao() {
+        return dataFinalizacao;
+    }
+
+    public void setDataFinalizacao(Calendar dataFinalizacao) {
+        if (dataFinalizacao != null) {
+            finalizado = true;
+        }
+        this.dataFinalizacao = dataFinalizacao;
+    }
 
 }
